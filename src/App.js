e5a92d425a393afbd3924b849bb19ef7f92e23b7
IvanGatsinski <playmodge@gmail.com>
2019-12-08 11:35:27 +0200

    moved the app-router components in routes file instead putting them all together

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import MainContainer from './components/MainContainer';
 import Navigation from './components/common/Navigation';
 import Footer from './components/common/Footer';
-import Home from './components/common/Home';
-import Login from './components/auth/login/Index';
-import Register from './components/auth/register/Index';
-import CreateRecipe from './components/recipe/Create';
-import ViewRecipe from './components/recipe/View';
-import EditRecipe from './components/recipe/Edit';
-import UserRecipes from './components/recipe/UserRecipes';
-import EditComment from './components/comments/Edit';
-import PrivateRoute from './components/helpers/PrivateRoute';
-import NotFound from './components/404/NotFound';
+import RouterComponents from './routes';
 import Provider from './providers/index';
-require('dotenv').config();
 
 function App() {
 
@@ -25,20 +15,7 @@ function App() {
       <Provider>
         <Navigation />
           <MainContainer>
-            <Switch>
-              <Route path="/login" component={Login} />
-              <Route path="/logout" render={props =>
-                <Redirect to="/login" />
-              } />
-              <Route path="/register" component={Register} />
-              <Route path="/" exact component={Home} />
-              <PrivateRoute path="/recipe/create" component={CreateRecipe} />
-              <PrivateRoute path="/recipe/:id/view" component={ViewRecipe} />
-              <PrivateRoute path="/recipe/:id/edit" component={EditRecipe} />
-              <PrivateRoute path="/comment/:id/edit" component={EditComment} />
-              <PrivateRoute path="/user/:id/recipes" component={UserRecipes} />
-              <Route component={NotFound} />
-            </Switch>
+            <RouterComponents/>
           </MainContainer>
         <Footer />
       </Provider>
--- src/routes.js
+++ src/routes.js
@@ -1,3 +1,35 @@
+import React from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
+import Home from './components/common/Home';
+import Login from './components/auth/login/Index';
+import Register from './components/auth/register/Index';
+import CreateRecipe from './components/recipe/Create';
+import ViewRecipe from './components/recipe/View';
+import EditRecipe from './components/recipe/Edit';
+import UserRecipes from './components/recipe/UserRecipes';
+import EditComment from './components/comments/Edit';
+import PrivateRoute from './components/helpers/PrivateRoute';
+import NotFound from './components/404/NotFound';
+
+export default function RouterComponents() {
+    return (
+        <Switch>
+            <Route path="/login" component={Login} />
+            <Route path="/logout" render={props =>
+                <Redirect to="/login" />
+            } />
+            <Route path="/register" component={Register} />
+            <Route path="/" exact component={Home} />
+            <PrivateRoute path="/recipe/create" component={CreateRecipe} />
+            <PrivateRoute path="/recipe/:id/view" component={ViewRecipe} />
+            <PrivateRoute path="/recipe/:id/edit" component={EditRecipe} />
+            <PrivateRoute path="/comment/:id/edit" component={EditComment} />
+            <PrivateRoute path="/user/:id/recipes" component={UserRecipes} />
+            <Route component={NotFound} />
+        </Switch>
+    )
+}
+
 export const routes = {
     login: () => { return "/login" },
     logout: () => { return "/logout" },
